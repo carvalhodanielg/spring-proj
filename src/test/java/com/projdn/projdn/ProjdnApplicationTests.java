d4ed7ffb73a023e96da52963bf7f3f6ad05de7e8
carvalhodanielg <carvalhodanielg@gmail.com>
2024-02-10 15:13:23 -0300

    first commit

---
+++ src/test/java/com/projdn/projdn/ProjdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projdn.projdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
