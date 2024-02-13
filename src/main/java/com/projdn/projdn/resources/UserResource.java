eab1ea9498730eb494228a85285cde42d2a1dcf2
carvalhodanielg <carvalhodanielg@gmail.com>
2024-02-13 18:49:06 -0300

    feat: user insert and delete

--- src/main/java/com/projdn/projdn/resources/UserResource.java
+++ src/main/java/com/projdn/projdn/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.projdn.projdn.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projdn.projdn.entities.User;
 import com.projdn.projdn.services.UserService;
@@ -31,6 +36,19 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User user = userService.findById(id);
 		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
