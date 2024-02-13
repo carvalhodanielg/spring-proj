fce5071374d3336ed6125a053fa47cfee7d71e54
carvalhodanielg <carvalhodanielg@gmail.com>
2024-02-13 17:44:00 -0300

    feat: assoiação pagamento

--- src/main/java/com/projdn/projdn/config/TestConfig.java
+++ src/main/java/com/projdn/projdn/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projdn.projdn.entities.Category;
 import com.projdn.projdn.entities.Order;
 import com.projdn.projdn.entities.OrderItem;
+import com.projdn.projdn.entities.Payment;
 import com.projdn.projdn.entities.Product;
 import com.projdn.projdn.entities.User;
 import com.projdn.projdn.entities.enums.OrderStatus;
@@ -83,7 +84,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
+	
 	}
 
 }
