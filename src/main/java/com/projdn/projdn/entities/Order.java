08d8903dee916cfcbc4fbe48207539827cc3f861
carvalhodanielg <carvalhodanielg@gmail.com>
2024-02-13 18:25:04 -0300

    feat: total e subtotal

--- src/main/java/com/projdn/projdn/entities/Order.java
+++ src/main/java/com/projdn/projdn/entities/Order.java
@@ -98,10 +98,18 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	public Set<OrderItem> getITems() {
+	public Set<OrderItem> getItems() {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
